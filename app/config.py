89728a2ab2d05061441b1757e07519b19a3e25b1
dev karan <devkaran7501@gmail.com>
2025-07-23 19:08:23 -0400

    Enabled alembic migrations

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings
+from pathlib import Path
 
 class Settings(BaseSettings):
     database_username : str
@@ -11,7 +12,7 @@ class Settings(BaseSettings):
     access_token_expire_minutes : int
 
     class Config:
-        env_file = ".env"
+        env_file = Path(__file__).parent / ".env"          # IMPORTANT: NOT HAVING EXACT FILE LOCATION WILL MESS UP ALEMBIC MIGRATIONS
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from .routers import post, user, auth, vote
 from .config import settings
 
 
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
@@ -22,7 +22,7 @@ app.include_router(auth.router)
 app.include_router(vote.router)
 
 
-# 10:06:00
+# 10:45:00
 
 
 
