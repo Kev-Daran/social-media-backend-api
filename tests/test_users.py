2153b352089aa735a5bfe604e7200afc8da53925
dev karan <devkaran7501@gmail.com>
2025-08-04 18:31:14 -0400

    Added test db

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,15 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
+from .database import client, session
 
 
-client = TestClient(app)
-
-def test_root():
+def test_root(client):
     res = client.get("/")
 
     assert res.json().get('message') == "API is active"
     assert res.status_code == 200
 
 
-def test_user_create():
+def test_user_create(client):
     res = client.post("/users/", json={"email" : "hello123@gmail.com", "password" : "password123"})
 
     new_user = schemas.UserOut(**res.json())
